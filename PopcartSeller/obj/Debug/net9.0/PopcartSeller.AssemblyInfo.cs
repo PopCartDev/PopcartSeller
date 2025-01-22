39f7f8b5e49307596e109d5e3b9093f5930e1660
herbertneutron <herbertonuoha@yahoo.com>
2025-01-22 12:26:53 +0000

    update fix

--- PopcartSeller/obj/Debug/net9.0/PopcartSeller.AssemblyInfo.cs
+++ PopcartSeller/obj/Debug/net9.0/PopcartSeller.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PopcartSeller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5801dbc73bdb14fe297d232a92773192c355108c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PopcartSeller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PopcartSeller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
