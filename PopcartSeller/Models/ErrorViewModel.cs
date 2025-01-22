5801dbc73bdb14fe297d232a92773192c355108c
herbertneutron <herbertonuoha@yahoo.com>
2025-01-22 01:45:23 +0000

    first commit

---
+++ PopcartSeller/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PopcartSeller.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
