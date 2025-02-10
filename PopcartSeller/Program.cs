dd57ffefb5b1da77b278dee210dc9edbd0db95a2
popcartdev <admin@popcart.africa>
2025-02-10 17:48:07 +0000

    updated program.cs

--- PopcartSeller/Program.cs
+++ PopcartSeller/Program.cs
@@ -29,6 +29,10 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://0.0.0.0:{port}");
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
